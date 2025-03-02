4879e35ec256702b4dc03ed81fb477c05061bfc7
vladislav_eroshkin <stiff_j@mai.ru>
2025-03-02 18:01:57 +0300

    old data removed before parsing folder

--- .venv/file_operations.py
+++ .venv/file_operations.py
@@ -60,45 +60,58 @@ def parse_folder(text_widget, table):
     text_widget_update(text_widget, "Начинается обработка файлов в папке...\n")
     clear_statistics()
 
-    start_time = time.time()
+    global global_card_counter
+    global_card_counter.clear()
 
+    files_to_process = get_files_to_process(folder_path)
+
+    start_time = time.time()
+    process_files_in_threads(files_to_process, user_name, text_widget)
+
+    save_converted_data(global_card_counter)
+    elapsed_time = time.time() - start_time
+    text_widget_update(text_widget, f"Обработка всех файлов завершена за {elapsed_time:.2f} секунд.\n")
+
+    table.update_statistics(text_widget)
+
+
+def get_files_to_process(folder_path):
     files_to_process = []
     for root, _, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.xml'):
                 files_to_process.append(os.path.join(root, file))
+    return files_to_process
 
-    def process_file(file_path):
-        try:
-            matches = extract_card_data(file_path, user_name)
-            local_counter = Counter()
 
-            for match in matches:
-                converted_match = convert_card_format(match.strip())
-                if converted_match:
-                    local_counter[converted_match] += 1
-
-            with lock:
-                global_card_counter.update(local_counter)
-
-        except Exception as e:
-            text_widget_update(text_widget, f"Ошибка при обработке файла {file_path}: {e}\n")
-
+def process_files_in_threads(files_to_process, user_name, text_widget):
     threads = []
     for file_path in files_to_process:
-        thread = threading.Thread(target=process_file, args=(file_path,))
+        thread = threading.Thread(target=process_file, args=(file_path, user_name, text_widget))
         threads.append(thread)
         thread.start()
 
     for thread in threads:
         thread.join()
 
-    save_converted_data(global_card_counter)  # Записываем после завершения всех потоков
 
-    elapsed_time = time.time() - start_time
-    text_widget_update(text_widget, f"Обработка всех файлов завершена за {elapsed_time:.2f} секунд.\n")
+def process_file(file_path, user_name, text_widget):
+    try:
+        matches = extract_card_data(file_path, user_name)
+        local_counter = Counter()
 
-    table.update_statistics(text_widget)
+        for match in matches:
+            converted_match = convert_card_format(match.strip())
+            if converted_match:
+                local_counter[converted_match] += 1
+
+        with lock:
+            global global_card_counter
+            global_card_counter.update(local_counter)
+
+    except Exception as e:
+        text_widget_update(text_widget, f"Ошибка при обработке файла {file_path}: {e}\n")
+
 
 def extract_card_data(file_path, user_name):
     pattern = fr'<cards[^>]*\bplayer="{user_name}"[^>]*>(.*?)</cards>'
